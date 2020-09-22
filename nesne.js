5e48353248f5abcc1f9118b28d15dad74863d91c
ilaydaOzdemir <ilayda.ozdemir2@ogr.sakarya.edu.tr>
2020-09-23 00:34:36 +0300

    nesne olusturma

---
+++ nesne.js
@@ -0,0 +1,11 @@
+//nesne oluşturma örnek
+var ogr = {
+    ad:"ilayda",
+    soyad:"özdemir",
+    okul:"Sakarya Üniversitesi",
+    durum:function(){
+        console.log("Bilgisayar Mühendisi");
+    }
+};
+ogr.mezunTarihi=2020;
+console.log(ogr);
