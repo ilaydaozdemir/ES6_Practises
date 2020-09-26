0dd94637d568bd802849f4894d247eac10bdccdc
ilaydaOzdemir <ilayda.ozdemir2@ogr.sakarya.edu.tr>
2020-09-26 23:45:58 +0300

    no message

---
+++ let-const.js
@@ -0,0 +1,15 @@
+/* 
+let anahtar kelimesiyle tanımladıgımız bir değişkenin değerini
+değiştirebilmek için;==>
+*/
+let x="bu bir değişkendir";
+x="declared edilmiş hali"
+console.log(x);
+
+/*
+const kullanımı
+*/
+const degerler=[1,2,3,4];
+// degerler=[5]; diyemeyiz hata verir
+degerler.push(5);
+console.log(degerler);
