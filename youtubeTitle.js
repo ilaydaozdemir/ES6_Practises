aad36eb35516215fb4ecbcc36fa4337a289b8612
ilaydaOzdemir <ilayda.ozdemir2@ogr.sakarya.edu.tr>
2020-09-27 12:47:03 +0300

    node modules ekleme

---
+++ youtubeTitle.js
@@ -0,0 +1,5 @@
+var getYoutubeTitle = require('get-youtube-title')
+ 
+getYoutubeTitle('fOOpi51B1lQ', function (err, title) {
+  console.log(title) // 'SLCHLD - EMOTIONS (feat. RIPELY) (prod. by GILLA)'
+})
