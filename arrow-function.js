c774ff729c2d6a90a5b0c5c24295c8722f542555
ilaydaOzdemir <ilayda.ozdemir2@ogr.sakarya.edu.tr>
2020-09-28 14:34:25 +0300

    fonksiyon tanımlama

---
+++ arrow-function.js
@@ -0,0 +1,14 @@
+//fonksiyon tanımlama
+const fonksiyonum=(sayi1,sayi2)=>{
+return sayi1+sayi2;
+};
+console.log(fonksiyonum(12,8));
+
+//sadelestırelım
+//return işlmei varsa bu sadelestırmeyı kullanabilirim
+const fonksiyonum1=(sayi3,sayi4)=>sayi3+sayi4;
+console.log(fonksiyonum1(5,7));
+
+//tek parametre varsa
+const fonksiyonum2=sayi5=>sayi5+19;
+console.log(fonksiyonum2(1));
