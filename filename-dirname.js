3f6dd7bc5319bb64f47b7ca3421d2411741ab766
ilaydaOzdemir <ilayda.ozdemir2@ogr.sakarya.edu.tr>
2020-09-30 20:08:13 +0300

    üzerinde caşıstıgım dosyanın nerde oldugunu ogrenmek ısteyebılırım

---
+++ filename-dirname.js
@@ -0,0 +1,4 @@
+//üzerinde caşıstıgım dosyanın nerde oldugunu ogrenmek ısteyebılırım
+
+console.log(__filename); //içinde bulundugum dosya nerdeyse adıyla bırlıkte bana dondurecek
+console.log(__dirname);
